2b932deba423266b1ac1a6c9ebbb4dd20d47d026
Pavla Hrabcová <pavla.hrabcova@avast.com>
2022-11-26 10:22:56 +0100

    greedy money change done

---
+++ 01 Algorithmic Toolbox/money_change_problem.py
@@ -0,0 +1,19 @@
+def money_change(a: int) -> list:
+    change = []
+    denominators = [10, 5, 1]
+    while a > 0:
+        for b in denominators:
+            while a:
+                if a >= b:
+                    change.append(b)
+                    a -= b
+                else:
+                    break
+
+    return change
+
+
+if __name__ == '__main__':
+    a = int(input())
+    print(money_change(a))
+
