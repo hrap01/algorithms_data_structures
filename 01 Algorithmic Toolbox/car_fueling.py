395b88ff7c920e047d4c7e23c747d22e4e4b5605
Pavla Hrabcová <pavla.hrabcova@avast.com>
2022-11-26 21:03:11 +0100

    for cycle with i

---
+++ 01 Algorithmic Toolbox/car_fueling.py
@@ -0,0 +1,50 @@
+from sys import stdin
+
+
+def min_refills(distance: int, tank: int, stops: list) -> int:
+    tank_full = tank
+    number_of_stops = 0
+    stops.append(distance)
+    if not stops and (distance > tank):
+        return -1
+    elif tank < stops[0]:
+        return -1
+    else:
+        stops_distance = []
+        previous_stop = 0
+        for current_stop in stops:
+            stops_distance.append(current_stop-previous_stop)
+            previous_stop = current_stop
+        for stop in stops_distance:
+            if stop > tank:
+                return -1
+        stops_distance.pop()
+        while distance > 0:
+            if distance <= tank:
+                return number_of_stops
+            for i in range(0, len(stops_distance)):
+                if tank > stops_distance[i]:
+                    tank -= stops_distance[i]
+                    distance -= stops_distance[i]
+                    i += 1
+                elif tank == stops_distance[i]:
+                    tank -= stops_distance[i]
+                    distance -= stops_distance[i]
+                    if distance == 0:
+                        return number_of_stops
+                    number_of_stops += 1
+                    tank = tank_full
+                    i += 1
+                elif tank < stops_distance[i]:
+                    number_of_stops += 1
+                    tank = tank_full
+                    tank -= stops_distance[i]
+                    distance -= stops_distance[i]
+                    i += 1
+
+
+
+
+if __name__ == '__main__':
+    d, m, _, *stops = map(int, stdin.read().split())
+    print(min_refills(d, m, stops))
