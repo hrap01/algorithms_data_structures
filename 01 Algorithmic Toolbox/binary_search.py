d8a229ce7eb04ac397011d679fbfd0823a365977
Pavla Hrabcová <pavla.hrabcova@avast.com>
2022-11-29 13:38:31 +0100

    final script

--- 01 Algorithmic Toolbox/binary_search.py
+++ 01 Algorithmic Toolbox/binary_search.py
@@ -1,28 +1,22 @@
-def binary_search_list(keys: list, query: list) -> list:
+def binary_search_list(keys: list, query: list) -> str:
     return_list = []
     max_number = len(keys) - 1
     min_number = 0
-    mid_number = (min_number + max_number) // 2
     for q in query:
-        def binary_search_number(keys: list, q: int, min_number: int, max_number: int, mid_number: int):
+        def binary_search_number(keys: list, q: int, min_number: int, max_number: int):
             nonlocal return_list
-            if q < keys[min_number]:
-                return_list.append(-1)
-            elif q > keys[max_number]:
-                return_list.append(-1)
-            elif q == keys[mid_number]:
-                return_list.append(mid_number)
-            elif q < keys[mid_number]:
-                #max_number = mid_number
-                binary_search_number(keys, q, min_number, max_number, mid_number-1)
+            if max_number >= min_number:
+                mid_number = (min_number + max_number) // 2
+                if q == keys[mid_number]:
+                    return_list.append(mid_number)
+                elif q < keys[mid_number]:
+                    binary_search_number(keys, q, min_number, mid_number-1)
+                else:
+                    binary_search_number(keys, q, mid_number+1, max_number)
             else:
-                #min_number = mid_number
-                binary_search_number(keys, q, min_number, max_number, mid_number+1)
-
-        binary_search_number(keys=keys, q=q, min_number=min_number, max_number=max_number, mid_number=mid_number)
-    return return_list
-
-
+                return_list.append(-1)
+        binary_search_number(keys=keys, q=q, min_number=min_number, max_number=max_number)
+    return ' '.join(str(n) for n in return_list)
 
 
 if __name__ == '__main__':
@@ -35,5 +29,3 @@ if __name__ == '__main__':
     assert len(input_queries) == num_queries
 
 print((binary_search_list(input_keys, input_queries)))
-    #for q in input_queries:
-    #    print(binary_search_list(input_keys, q), end=' ')
