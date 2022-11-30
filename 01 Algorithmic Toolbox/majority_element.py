21589ac9f2fee9f4d9e7be5f03feab4da12a4a85
Pavla Hrabcová <pavla.hrabcova@avast.com>
2022-11-30 12:44:41 +0100

    working but slow

--- 01 Algorithmic Toolbox/majority_element.py
+++ 01 Algorithmic Toolbox/majority_element.py
@@ -1,9 +1,21 @@
-def majority_element_naive(elements):
-    for e in elements:
-        if elements.count(e) > len(elements) / 2:
-            return 1
-
-    return 0
+def majority_element_naive(elements: list):
+    highest_value = 0
+    comparison_value = 0
+    comparison_length = len(elements)/2
+    for number in elements:
+        for value in elements:
+            if number == value:
+                comparison_value += 1
+                #elements.remove(value)
+        if comparison_value > highest_value:
+            highest_value = comparison_value
+            comparison_value = 0
+        else:
+            comparison_value = 0
+    if highest_value > comparison_length:
+        return 1
+    else:
+        return 0
 
 
 if __name__ == '__main__':
